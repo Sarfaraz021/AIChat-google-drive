6ec0d5264492e27a8ceff29cb4256cf6199bb8c9
Sarfaraz Ahmed <20cs067@students.muet.edu.pk>
2024-07-02 01:49:02 +0500

    inshaaAllah

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-# #!/bin/sh
-# # Ensure .credentials directory exists
-# mkdir -p /home/appuser/.credentials
-# # Copy credentials.json to the expected directory
-# cp .credentials/credentials.json /home/appuser/.credentials/credentials.json
+#!/bin/sh
+# Ensure .credentials directory exists
+mkdir -p /home/appuser/.credentials
+# Copy credentials.json to the expected directory
+cp .credentials/credentials.json /home/appuser/.credentials/credentials.json
